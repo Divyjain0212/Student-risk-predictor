45447315964282447047e758816d7f55864fcd27
Divyjain0212 <divyj12349@gmail.com>
2025-10-03 20:28:48 +0530

    fix: update import paths to be relative

--- app/api/students/[id]/route.ts
+++ app/api/students/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
-import connectToDatabase from '@/lib/mongodb';
-import { Student, Attendance, Assessment, FeePayment, RiskScore } from '@/models';
+import connectToDatabase from '../../../../lib/mongodb';
+import { Student, Attendance, Assessment, FeePayment, RiskScore } from '../../../../models';
 
 export async function GET(
   request: NextRequest,
